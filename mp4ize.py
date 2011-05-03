7316571fbe37922ec3cc52b1f733877b965ca73d
anchepiece <anchepiece@gmail.com>
2011-05-03 09:46:09 -0400

    added iphone4 option

--- mp4ize.py
+++ mp4ize.py
@@ -25,6 +25,9 @@
 #                removed bogus 'here' debug statement
 # Oct. 27, 2010: assume ffmpeg 0.6: use libxvid and libfaac by default,
 #                add "k" to -bufsize, -ab, and -b parameters.
+# May   2, 2011: added iphone4 option
+#                use ffmpeg, not /usr/bin/ffmpeg
+# May   3, 2011: changed codec from libfaac to aac
 #
 # This program is free software. You may distribute it under the terms of
 # the GNU General Public License as published by the Free Software
@@ -111,17 +114,25 @@ import fcntl, select
 
 ##############################################################################
 
-DEFAULT_ARGS = "-f mp4 -y -vcodec libxvid -maxrate 1000 -mbd 2 -qmin 3 -qmax 5 -g 300 -acodec libfaac"
-DEFAULT_BUFSIZE = "4096k"
-DEFAULT_AUDIO_BITRATE = "128k"
-DEFAULT_VIDEO_BITRATE = "400k"
-DEFAULT_OUTDIR = "./"
+DEFAULT_BIN = 'ffmpeg'
+DEFAULT_ARGS = '-f mp4 -y -vcodec libxvid -maxrate 1000 -mbd 2' + \
+               ' -qmin 3 -qmax 5 -g 300 '
+#CODEC_AAC = '-acodec libfaac'
+CODEC_AAC = '-acodec aac -strict experimental'
+DEFAULT_ARGS += CODEC_AAC
+
+DEFAULT_BUFSIZE = '4096k'
+DEFAULT_AUDIO_BITRATE = '128k'
+DEFAULT_VIDEO_BITRATE = '400k'
+DEFAULT_OUTDIR = './'
 IPOD_WIDTH = 320.0
 IPOD_HEIGHT = 240.0
 IPHONE_WIDTH = 480.0
 IPHONE_HEIGHT = 320.0
-
+IPHONE4_WIDTH = 960.0
+IPHONE4_HEIGHT = 640.0
     
+_prog = sys.argv[0]
 __version__ = '1.0'
 __usage__ = \
 """
@@ -174,33 +185,25 @@ def process_files(files, options):
         return
 
     print 'Found %s files to be processed' % len(files)
-
     for f in files:
         print '    processing: %s' % f    
         outfile = os.path.splitext(os.path.basename(f))[0] + '.mp4'
         outfile = os.path.join(options.outdir, outfile)
         print '    outfile:', outfile        
 
-        #ext = os.path.splitext(f)[1]
-        #print '    {0} {1} {2}'.format(f, outfile, ext)
-
         # open the file to figure out the aspect ratio
         duration, time_f, w, h = 0.0, 0.0, 0, 0
-        cmd = '/usr/bin/ffmpeg -i "%s" ' % f
+        cmd = DEFAULT_BIN + ' -i "%s" ' % f
         print cmd
         print ''
         returncode, output = encode(cmd)
-        
-#        p = Popen ([cmd, '-i', f], stdout=PIPE, stderr=PIPE)
-#        output = p.stderr.read()
-        #output = p.communicate()[1] #read in sterr
+
         pattern = re.compile('.*Invalid data.*')
         m = pattern.search(output)
         if not m:
             pattern = re.compile( r'Video:.+ (\d+)x(\d+)' )        
             m = pattern.search(output)
             if m:
-                #print 'got match test %s ' % m.group(0)
                 w = float(m.group(1))
                 h = float(m.group(2))
             
@@ -212,7 +215,8 @@ def process_files(files, options):
                 duration += float(m.group(3))
                 duration += float(m.group(4)) / 10                                             
             
-            print 'Got values from input: w=%s h=%s duration=%s' % (w, h, duration)
+            print 'Got values from input: w=%s h=%s duration=%s' % (
+                w, h, duration)
 
         if duration == 0 or w == 0 or h == 0:
             print output
@@ -232,8 +236,8 @@ def process_files(files, options):
         pad = abs(pad)
         padarg1, padarg2 = "padtop", "padbottom"
         
-        print aspect, user_width, user_height, width, height,  pad
-        print ''
+        #print aspect, user_width, user_height, width, height, pad
+        #print ''
         
         if os.path.exists(outfile):
             print 'Removing existing output: %s' % outfile
@@ -241,45 +245,42 @@ def process_files(files, options):
 #             if val != 'y':
 #                 sys.exit(1)            
             os.remove(outfile)
-        cmd = '/usr/bin/ffmpeg -i "{0}" {1} -bufsize {2} -s {3}x{4} -{5} {6} -{7} {6} -ab {8} -b {9} "{10}"'.format(
-            f, DEFAULT_ARGS, DEFAULT_BUFSIZE, width, height, padarg1, pad, padarg2, options.audio, options.video, outfile)
             
+        cmd = ('{0} -i "{1}" {2} -bufsize {3} -s {4}x{5} -{6} {7}' + \
+            ' -{8} {7} -ab {9} -b {10} "{11}"').format(
+            DEFAULT_BIN, f, DEFAULT_ARGS, DEFAULT_BUFSIZE, 
+            width, height, padarg1, pad, padarg2, 
+            options.audio, options.video, outfile)
         print cmd
         print ''
-        #p = Popen (cmd.split(), stdout=PIPE, stderr=PIPE)
         returncode, output = encode(cmd, duration)
-        #print 'asdf', returncode, output
-        
-        #output = p.communicate() #read in stout    
         if returncode == 0:
-            #time = verify_output(t)
-            #print 'time=', time
-            #print 'time=', verify_output(output)   
             time_f = verify_output(output)                
-            #print 'time=%s' % t                             
         else: 
             pattern = re.compile('.*Invalid pixel aspect ratio.*')
             if pattern.search(output):
                 # lets try again while forcing an aspect ratio
-                print 'Invalid pixel aspect ratio, running again with source ratio'
-                cmd = '/usr/bin/ffmpeg -i "{0}" {1} -bufsize {2} -s {3}x{4} -{5} {6} -{7} {6} -aspect {11} -ab {8} -b {9} "{10}"'.format(
-                    f, DEFAULT_ARGS, DEFAULT_BUFSIZE, width, height, padarg1, pad, padarg2, options.audio, options.video, outfile, aspect)
-
+                print 'Invalid pixel aspect ratio, ' + \
+                    'running again with source ratio'
+                cmd = ('{0} -i "{1}" {2} -bufsize {3} -s {4}x{5} -{6} {7}' + \
+                    ' -{8} {7} -aspect {12} -ab {9} -b {10} "{11}"').format(
+                     DEFAULT_BIN, f, DEFAULT_ARGS, DEFAULT_BUFSIZE, 
+                     width, height, padarg1, pad, padarg2, 
+                     options.audio, options.video, outfile, aspect)
                 print cmd
                 print ''
                 returncode, output = encode(cmd, duration)
                 if returncode == 0:
                     print 'Complete.'
                     time_f = verify_output(output)   
-                else:       
+                else:
                     print output  
-                    print 'Error: Video encoding failed'                
+                    print 'Error: Video encoding failed' + \
+                            ' (with forced aspect ratio)'                
             else:
                 print output
                 print 'Error: Video encoding failed'
 
-                
-        #"the line is %s" % line
         duration = float(duration)
         time_f = float(time_f)
         print "source duration: %s" % duration
@@ -295,7 +296,7 @@ def encode(cmd, duration=0):
     result = ''
     time_f = 0
     args = shlex.split(cmd)
-    p = Popen (args, stdout=PIPE, stderr=PIPE, shell=False)    
+    p = Popen (args, stdout=PIPE, stderr=PIPE, shell=False)
 
     fcntl.fcntl(
         p.stderr.fileno(),
@@ -315,19 +316,16 @@ def encode(cmd, duration=0):
                 time_f = tmp_time_f
                 if duration:
                     percent = (float(time_f)/float(duration))*100
-	                #printf "%2g ", percent
-                    #sys.stderr.write(' %2g Completed  [%s] \r' % (percent, time_f))
-                    sys.stderr.write('  %2d%% Completed [position %ss] \r' % (percent, time_f))                    
+                    sys.stderr.write('  %2d%% Completed [position %ss] \r'
+                         % (percent, time_f))                    
                 else:
                     sys.stderr.write(' time=%s \r' % time_f)
         time.sleep(.1)
     p.wait()
     return p.returncode, result
-    # http://derrickpetzold.com/index.php/capturing-output-from-ffmpeg-python/
     
 def verify_output(output):
     time_f = 0
-    #pattern = re.compile(r'time=(.*) bitrate')    
     pattern = re.compile( r'time=([^\s]+)' )
     if type(output).__name__ == 'tuple':            
         m = pattern.search(output[0])
@@ -339,9 +337,6 @@ def verify_output(output):
             print 'got result in stderr'        
             time_f = m.group(1)
     else:
-        #pattern = re.compile( r'time=(.*) [\w+]' )                    
-        #pattern = re.compile( r'time=([^\s]+) bitrate' )                 
-        #pattern = re.compile( r'time=([^\s]+)' )                 
         pattern = re.compile( r'time=([^\s]+)' )                                 
         for line in output.split('\n'):
             m = pattern.search(line)
@@ -352,12 +347,10 @@ def verify_output(output):
 ##############################################################################
 
 def main(argv):
-#    usage = "%prog [options]"
     usage = __usage__    
     description=usage
 
 
-    #http://docs.python.org/library/argparse.html#module-argparse
     def valid_dir(string):
         value = str(string)
         if not os.path.isabs(value):
@@ -367,42 +360,47 @@ def main(argv):
             raise argparse.ArgumentTypeError(msg)
         return value    
 
-    parser = argparse.ArgumentParser(description='encode videos to mp4 for an iPod or an iPhone')
-#    parser.add_argument('input', metavar='file', type=str, nargs='+',
-#                       help='list of file names to process')
+    parser = argparse.ArgumentParser(
+        description='encode videos to mp4 for an iPod or an iPhone')
 
-#    parser.add_argument('--version', action='version', version='%(prog)s %s' % __version__,
-#                        help='show program verison')
-
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0',
-                        help='show program verison')    
-    
-#    parser.add_argument('input', metavar=('file1.avi', 'file2.mpg file3.asf'), type=str, nargs='+', 
-#                       help='list of file names to process')
-
+    parser.add_argument('--version', action='version', 
+        version='%s %s' % (_prog, __version__),
+        help='show program verison')
     parser.add_argument('input', metavar=('file'), type=str, nargs='+', 
-                       help='list of file names to process')
-
-    # setting defaults creates a value in args, otherwise the value passed will be None
-    parser.add_argument('-a', '--audio', metavar='RATE', type=str, nargs=1, default=DEFAULT_AUDIO_BITRATE,
-                       help='override default audio bitrate (%(default)s)' )    
-    parser.add_argument('-b', '--video', metavar='RATE', type=str, nargs=1, default=DEFAULT_VIDEO_BITRATE,
-                       help='override default video bitrate (%(default)s)' )    
+        help='list of file names to process')
+        # setting defaults creates a value in args, 
+        #   otherwise the value passed will be None
+    parser.add_argument('-a', '--audio', metavar='RATE', type=str, nargs=1, 
+        default=DEFAULT_AUDIO_BITRATE,
+        help='override default audio bitrate (%(default)s)' )    
+    parser.add_argument('-b', '--video', metavar='RATE', type=str, nargs=1, 
+        default=DEFAULT_VIDEO_BITRATE,
+        help='override default video bitrate (%(default)s)' )    
     parser.add_argument('-v', '--verbose', action='store_true')   
+    parser.add_argument('-w', '--width', type=int, nargs=1, 
+        default=int(IPOD_WIDTH),
+        help='override default width (%(default)s)')    
+    parser.add_argument('-t', '--height', type=int, nargs=1, 
+        default=int(IPOD_HEIGHT),
+        help='override default height (%(default)s)' )
+    parser.add_argument('-i', '--iphone', action='store_true', 
+        default='%dx%d' % (IPHONE_WIDTH, IPHONE_HEIGHT),
+        help='use default iphone height and width (%(default)s)') 
+    parser.add_argument('-4', '--iphone4', action='store_true', 
+        default='%dx%d' % (IPHONE4_WIDTH, IPHONE4_HEIGHT),
+        help='use default iphone4 height and width (%(default)s)')     
 
-    parser.add_argument('-w', '--width', type=int, nargs=1, default=int(IPOD_WIDTH),
-                       help='override default width (%(default)s)')    
-    parser.add_argument('-t', '--height', type=int, nargs=1, default=int(IPOD_HEIGHT),
-                       help='override default height (%(default)s)' )    
-    parser.add_argument('-i', '--iphone', action='store_true')  
-      
-    parser.add_argument('-o', '--outdir', metavar='dir', type=valid_dir, nargs=1, default=DEFAULT_OUTDIR,
-                       help='write files to given directory')        
+    parser.add_argument('-o', '--outdir', metavar='dir', type=valid_dir, 
+        nargs=1, default=DEFAULT_OUTDIR,
+        help='write files to given directory')        
 
     args = parser.parse_args()
     if args.iphone:
         args.width = IPHONE_WIDTH
         args.height = IPHONE_HEIGHT
+    if args.iphone4:
+        args.width = IPHONE4_WIDTH
+        args.height = IPHONE4_HEIGHT        
     process(args)
 
 ##############################################################################
